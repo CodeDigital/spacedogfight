1d04304476d99565802774166d56a506e9a83f37
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-01-11 13:11:26 +0700

    Initial Commit 1.0.1 Pre Alpha

--- game/ships/fist/fist.lua
+++ game/ships/fist/fist.lua
@@ -10,15 +10,30 @@ fist.rocket = "na"
 fist.health = 500
 
 fist.load = function()
-  fist.spritesheet = love.graphics.newImage("fist.png")
+  fist.spritesheet = love.graphics.newImage("game/ships/fist/sprite.png")
   fist.idle = {}
   table.insert(fist.idle, love.graphics.newQuad(0, 0, 32, 32, fist.spritesheet:getDimensions()))
-  fist.flying = {}
-  table.insert(fist.flying, love.graphics.newQuad(32, 0, 32, 32, fist.spritesheet:getDimensions()))
-  table.insert(fist.flying, love.graphics.newQuad(64, 0, 32, 32, fist.spritesheet:getDimensions()))
-  table.insert(fist.flying, love.graphics.newQuad(96, 0, 32, 32, fist.spritesheet:getDimensions()))
-  table.insert(fist.flying, love.graphics.newQuad(128, 0, 32, 32, fist.spritesheet:getDimensions()))
-  table.insert(fist.flying, love.graphics.newQuad(160, 0, 32, 32, fist.spritesheet:getDimensions()))
+  fist.fly = {}
+  table.insert(fist.fly, love.graphics.newQuad(32, 0, 32, 32, fist.spritesheet:getDimensions()))
+  table.insert(fist.fly, love.graphics.newQuad(64, 0, 32, 32, fist.spritesheet:getDimensions()))
+  table.insert(fist.fly, love.graphics.newQuad(96, 0, 32, 32, fist.spritesheet:getDimensions()))
+  table.insert(fist.fly, love.graphics.newQuad(128, 0, 32, 32, fist.spritesheet:getDimensions()))
+  table.insert(fist.fly, love.graphics.newQuad(160, 0, 32, 32, fist.spritesheet:getDimensions()))
+  fist.flyFrame = 1
+  fist.currentFrame = fist.idle[1]
+end
+
+fist.nextFrame = function(type)
+  if(type == "idle")then
+    fist.currentFrame = fist.idle[1]
+  elseif(type == "fly")then
+    if(fist.flyFrame >= 4)then
+      fist.flyFrame = 1
+    else
+      fist.flyFrame = fist.flyFrame + 1
+    end
+    fist.currentFrame = fist.fly[fist.flyFrame]
+  end
 end
 
 fist.ability = function()
--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
+
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest", 1)
-  -- body...
 end
 
 function love.update(dt)
-  -- body...
 end
 
 function love.draw()
-  -- body...
 end
