347571cf7eb7e8f448b78250bbd821c1835283eb
DigitalData <legoxavierlocketravers@gmail.com>
2018-04-17 18:39:15 +1000

    Started 0.0.1

---
+++ game/physics.lua
@@ -0,0 +1,21 @@
+physics = {}
+
+physics.forward = function(player)
+return player
+end
+
+physics.backward = function(player)
+return player
+end
+
+physics.left = function(player)
+
+
+  return player
+end
+
+physics.right = function(player)
+return player
+end
+
+return physics
